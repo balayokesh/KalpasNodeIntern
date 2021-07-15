ea7d813c7ea75c2aa3e73f493ad2a7b82b3517e0
Bala Yokesh Mani A <balayokeshmani@gmail.com>
2021-07-15 17:24:00 +0530

    problem in getting file upload URL

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
+const fs = require('fs');
+const csv = require('csv-parser');
 
-router.get('/', (req, res) => {
-    res.send('test success');
+router.post('/', (req, res) => {
+    const results = [];
+    const filePath = 'test.csv';
+    fs.createReadStream(filePath)
+        .pipe(csv({}))
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
+            res.send(results);
+        });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser')
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 
 // Configure routes
 const indexRouter = require('./routes/index');
