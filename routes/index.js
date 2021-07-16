df78814d5a07db36eb7ce4a79c721a87f5d2bd0f
Bala Yokesh Mani A <balayokeshmani@gmail.com>
2021-07-16 11:10:37 +0530

    Implemented read, update operation

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const csv = require('csv-parser');
 // Import Models
 const Employee = require('../models/employee.model');
 
+// Create
 router.post('/', (req, res) => {
     const results = [];
     const filePath = req.body.filePath;
@@ -28,10 +29,31 @@ router.post('/', (req, res) => {
     );
 });
 
+// Read
+router.get('/:id', (req, res) => {
+    Employee.findById(req.params.id)
+        .then(emp => res.json(emp))
+        .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+// Update
+router.post('/edit/:id', (req, res) => {
+    Employee.findById(req.params.id)
+        .then(employee => {
+            employee.name = req.body.name;
+            employee.age = req.body.age;
+            employee.save()
+                .then(() => res.json('Employee details deleted'))
+                .catch(err => res.status(400).json(`Error: ${err}`));
+        })
+})
+    
+
+// Delete
 router.delete('/:id', (req, res) => {
     Employee.findByIdAndDelete(req.params.id)
         .then(() => res.json('Employee details deleted'))
         .catch(err => res.status(400).json(`Error: ${err}`));
-})
+});
 
 module.exports = router;
